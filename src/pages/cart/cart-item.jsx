231d46a3ba772611cdbf29c0e650a21969e90cbe
naveenmech <vas.naveenkumar99@gmail.com>
2023-04-25 15:16:52 +0530

    changed minor errors

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -8,7 +8,7 @@ export const CartItem = (props) => {
 
   return (
     <div className="cartItem">
-      <img src={productImage} />
+      <img src={productImage} alt="IMAGESW"/>
       <div className="description">
         <p>
           <b>{productName}</b>
